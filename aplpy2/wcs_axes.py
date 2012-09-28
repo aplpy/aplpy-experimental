19ba14a4df8421617e6a61d06da8236e11b281b7
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-09-28 14:22:40 -0400

    Allow WCS object to initialize WCSAxes

--- aplpy2/wcs_axes.py
+++ aplpy2/wcs_axes.py
@@ -7,19 +7,27 @@ from .transforms.wcs import WcsPixel2WorldTransform
 class WCSAxes(HostAxes):
 
     def __init__(self, fig, rect, input):
-        
+
         if isinstance(input, basestring) and input.lower().endswith('.fits'):
-            
+
             # Read in image
             hdu = fits.open(input)[0]
 
             # Set up WCS transformation
             wcs = WCS(hdu.header)
             wcs_trans = WcsPixel2WorldTransform(wcs)
-            
+
             # Initalize axes
             HostAxes.__init__(self, fig, rect, wcs_trans, xcoord_type='longitude', ycoord_type='latitude')
-            
+
+        elif isinstance(input, WCS):
+
+            # Set up WCS transformation
+            wcs_trans = WcsPixel2WorldTransform(input)
+
+            # Initalize axes
+            HostAxes.__init__(self, fig, rect, wcs_trans, xcoord_type='longitude', ycoord_type='latitude')
+
         else:
-            
+
             raise Exception("invalid type")
